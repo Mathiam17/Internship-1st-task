8d0cc222601f5de0afa87f90d4fe377e3fafee19
Matīss Langenfelds <57113971+Mathiam17@users.noreply.github.com>
2019-11-28 18:11:42 +0200

    Add files via upload
    Added function to create new currencies instead of displaying the same array in different class

--- Internship Homework (New Version)/Internship_Homework.js
+++ Internship Homework (New Version)/Internship_Homework.js
@@ -2,15 +2,15 @@ new Vue ({
     el: "#Menu",
     data: {
         Currencies: [
-            {Text:'EUR', id: 'Eur', checkbox_id: 'CheckEUR', isActive: true, checked: false, seen: false},
-            {Text:'PLN', id: 'Pln', checkbox_id: 'CheckPLN', isActive: true, checked: false, seen: false},
-            {Text:'GEL', id: 'Gel', checkbox_id: 'CheckGEL', isActive: true, checked: false, seen: false},
-            {Text:'DKK', id: 'Dkk', checkbox_id: 'CheckDKK', isActive: true, checked: false, seen: false},
-            {Text:'CZK', id: 'Czk', checkbox_id: 'CheckCZK', isActive: true, checked: false, seen: false},
-            {Text:'GBP', id: 'Gbp', checkbox_id: 'CheckGBP', isActive: true, checked: false, seen: false},
-            {Text:'SEK', id: 'Sek', checkbox_id: 'CheckSEK', isActive: true, checked: false, seen: false},
-            {Text:'USD', id: 'Usd', checkbox_id: 'CheckUSD', isActive: true, checked: false, seen: false},
-            {Text:'RUB', id: 'Rub', checkbox_id: 'CheckRUB', isActive: true, checked: false, seen: false}
+            {Text:'EUR', id: 'Eur', checkbox_id: 'CheckEUR', isActive: false, checked: false},
+            {Text:'PLN', id: 'Pln', checkbox_id: 'CheckPLN', isActive: false, checked: false},
+            {Text:'GEL', id: 'Gel', checkbox_id: 'CheckGEL', isActive: false, checked: false},
+            {Text:'DKK', id: 'Dkk', checkbox_id: 'CheckDKK', isActive: false, checked: false},
+            {Text:'CZK', id: 'Czk', checkbox_id: 'CheckCZK', isActive: false, checked: false},
+            {Text:'GBP', id: 'Gbp', checkbox_id: 'CheckGBP', isActive: false, checked: false},
+            {Text:'SEK', id: 'Sek', checkbox_id: 'CheckSEK', isActive: false, checked: false},
+            {Text:'USD', id: 'Usd', checkbox_id: 'CheckUSD', isActive: false, checked: false},
+            {Text:'RUB', id: 'Rub', checkbox_id: 'CheckRUB', isActive: false, checked: false}
         ],
         selectedCurrencies: []
     },
@@ -21,6 +21,16 @@ new Vue ({
             }else{
                 Currency.isActive = true;
             } 
+        },
+        addCurrency(Currency, index) {
+            if (Currency == true) {
+                this.selectedCurrencies.push(this.Currencies[index])
+            } else if (Currency == false) {
+                this.$delete(this.selectedCurrencies, index)
+            }
+        },
+        removeCurrency(index) {
+            this.$delete(this.selectedCurrencies, index)
         },
         toggleCheckBox(Checkbox) {	// Funkcija kas maina checkboxa statusu uz izveletu vai neizveletu
             if (Checkbox.checked == false) {
